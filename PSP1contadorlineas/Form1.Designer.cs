c312519c628aac299c032aea35c46e1cfbf8e2ab
nicolasTovar <jntovar5@misena.edu.co>
2019-12-12 20:03:53 -0500

    PSP1 c#

---
+++ PSP1contadorlineas/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace PSP1contadorlineas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Class = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Proxy = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Items = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Locs = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Class,
+            this.Proxy,
+            this.Items,
+            this.Locs});
+            this.dataGridView1.Location = new System.Drawing.Point(269, 97);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(496, 285);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // Class
+            // 
+            this.Class.HeaderText = "Clase";
+            this.Class.Name = "Class";
+            // 
+            // Proxy
+            // 
+            this.Proxy.HeaderText = "Proxys";
+            this.Proxy.Name = "Proxy";
+            // 
+            // Items
+            // 
+            this.Items.HeaderText = "Items";
+            this.Items.Name = "Items";
+            // 
+            // Locs
+            // 
+            this.Locs.HeaderText = "Locs";
+            this.Locs.Name = "Locs";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Examinar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(69, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Calcular";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Class;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Proxy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Items;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Locs;
+    }
+}
+
---
+++ PSP1contadorlineas/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;   
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PSP1contadorlineas
+{
+    public partial class Form1 : Form
+    {
+        int contar;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog buscar = new OpenFileDialog();
+            buscar.ShowDialog();
+            string texto1 = buscar.FileName.ToString();
+            string linea = "";
+            StreamReader leer = new StreamReader(texto1);
+            string nombre = buscar.SafeFileName;
+            int contador = 0;
+            int contarMetodo = 0;
+            string comentario = "//";
+            string comenInicio = "/*";
+            string comenFinal = "*/";
+            string metodo = "public";
+            
+            while ((linea = leer.ReadLine()) != null)
+            {
+                if (linea != "") 
+                {
+                    contador++;
+                }
+                if (linea.Contains(comentario) == true ) 
+                {
+                    contador--;
+                }
+                if ((linea.Contains(comenInicio) == true)&&(linea.Contains(comenFinal)==true))
+                {
+                    contador--;
+                }
+                else if (linea.Contains(comenInicio)== true)
+                {
+                    contador--;
+                    while ((linea = leer.ReadLine()) != comenFinal)
+                    {
+
+                    }
+                }
+                if (linea.Contains(metodo) == true)
+                {
+                    contarMetodo++;
+                }
+            }
+            /*while ((linea = leer.ReadLine()) != null)
+            {
+                if (linea.Contains(metodo) == true)
+                {
+                    contarMetodo++;
+                }
+            }*/
+                dataGridView1.Rows.Add(nombre, "", contarMetodo, contador);
+            contar = contar + contador;
+            //dataGridView1.Rows.Add("","", "", contar);
+            
+
+        }
+        public void numeroMetodo()
+        {
+
+        }
+        public void sumar()
+        {
+            //int n
+            
+            dataGridView1.Rows.Add("", "", "", contar);
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sumar();
+        }
+    }
+}
